360f2eb2c76eb365545c6e708dc72719dd2a950b
Augustė Nausėdaitė <auguste.nausedaite@student.auc.nl>
2016-11-25 10:55:21 +0100

    spotify authorizarion is set up

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const express = require ('express')
 const session = require ('express-session')
 const bodyParser = require('body-parser')
 //const fbData = require( __dirname + '/fb-data')
-//const spotData = require( __dirname + '/spotify-data')
+let spotifyData = require( __dirname + '/spotify-data' )
+
 const app = express()
 
 // set up the views engine
@@ -12,19 +13,18 @@ app.set('views', './views')
 app.set('view engine', 'pug')
 
 // For logged in user start session
-app.use(
-	express.static( 'static' ),
-	session ({
-		secret: 'this is some secret',
-		resave: true,
-		saveUninitialized: false
-	})
-)
+// app.use(
+// 	express.static( 'static' ),
+// 	session ({
+// 		secret: 'this is some secret',
+// 		resave: true,
+// 		saveUninitialized: false
+// 	})
+// )
 
-// trial route
-app.get( '/ping', ( req, res ) => {
-	res.send( 'pong' )
-})
+app.use(express.static(__dirname + '/public'))
+
+app.use('/', spotifyData)
 
 app.listen ( 8000, () => {
 	console.log ( 'up and running ')
